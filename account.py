9149a0912cacbae503c9c5f8d436209e13b4351b
Shaline Davida <shalinedavida@gmail.com>
2025-05-27 15:37:50 +0300

    added new changes to the file

--- account.py
+++ account.py
@@ -1,10 +1,14 @@
 class Account:
+    interest_rate = 0.05
     def __init__(self,name):
         self.name=name
         self.balance=0
         self.deposits=[]
         self.withdrawals=[]
         self.transaction=[]
+        self.loan-limit=loan-limit
+        self.loan_amount=0
+        self.frozen=False
     def deposit(self,amount):
         if amount > 0:
             self.balance += amount
@@ -28,14 +32,38 @@ class Account:
             return f"{amount} has been transferred successfully"
     def get_balance(self):
         return sum(self.transaction) 
-    def request_loan(self):
+    def request_loan(self,amount):
+        if amount >self.loan_limit:
+            return f"Your loan request has been denied"
+        else:
+            self.loan_amount += amount
+            self.balance += amount  
+            return f"Loan of {amount} approved .New balance is {self.balance}"  
         
     def repay_loan(self):
-    def view_account(self):           
-    def change_owner(self):
+        self.loan_amount -= amount
+        self.balance -= amount
+        return f"Loan repayment is successful "
+    def view_account(self,name,balance): 
+        return f"Hello {self.name} your current account balance is {self.balance}"
+    def change_owner(self,new_name):
+        self.name=new_name
+        return f"The new account owner is {self.name}"
     def account_statement(self):
-    def interest(self):
-    def freeze(self):
-    def minimum_balance(self):
-    def close_account(self):
+        for transaction in transactions:
 
+
+    def interest(self):
+       interest= self.balance * self.interest_rate
+       self.balance += interest
+       return f"This is the new amount {self.balance}"
+    def freeze(self):
+        self.frozen = True
+        return f"This account is frozen"
+    def unfreeze(self):
+        self.frozen= False  
+        return f" This account is now unfrozen"    
+    def minimum_balance(self):
+        
+
+    def close_account(self):
